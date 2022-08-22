198f632d31db19e23835290ad1af68cb7d1a5505
joao martins <joaomgwork@hotmail.com>
2022-08-22 17:33:29 +0100

    added default value to pending column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_162218) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_162736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_162218) do
     t.bigint "board_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.index ["board_id"], name: "index_bookings_on_board_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
