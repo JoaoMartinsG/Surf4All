32b35d309fc0b0104e7ef713976fc68d3bb21f5d
Alexandrejfcr <alexandre_fcr@hotmail.com>
2022-08-23 16:04:26 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
-<<<<<<< HEAD
+  def dashboard
+    #test
+  end
 
-  def dashboard
-    @boards = Board.all
-  end
-=======
-  def dashboard
-    
-  end
-  
->>>>>>> a9487b28b4b8669b5888622ab8c79307e4ce00d5
 end
